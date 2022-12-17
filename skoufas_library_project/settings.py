7da91d751c29f60f8103b172e98a16e590cf9afd
Claudio Bantaloukas <rockdreamer@gmail.com>
2022-12-17 10:04:40 +0000

    Spacing and checks

--- skoufas_library_project/settings.py
+++ skoufas_library_project/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
 import os
 from pathlib import Path
+
 from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
--- skoufas_library_project/urls.py
+++ skoufas_library_project/urls.py
@@ -1,4 +1,4 @@
-"""skoufas_library_project URL Configuration
+"""skoufas_library_project URL Configuration.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import include
+from django.urls import path
 
 urlpatterns = [
     path("", include("books.urls")),
