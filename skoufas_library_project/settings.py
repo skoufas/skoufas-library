e8dc7d325bed1d5e8803a31077d4aa632dedd135
Claudio Bantaloukas <rockdreamer@gmail.com>
2022-12-26 21:29:36 +0000

    Add devcontainer

--- skoufas_library_project/settings.py
+++ skoufas_library_project/settings.py
@@ -56,29 +56,44 @@ LOGGING = {
 
 # Application definition
 
-INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "bootstrap5",
-    "books",
-    "djangoql",
-    "loaning",
-]
+INSTALLED_APPS = (
+    [
+        "django.contrib.admin",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.sessions",
+        "django.contrib.messages",
+        "django.contrib.staticfiles",
+        "bootstrap5",
+        "books",
+        "djangoql",
+        "loaning",
+    ]
+    + [
+        "django_extensions",
+    ]
+    if DEBUG
+    else []
+)
 
-MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.locale.LocaleMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-]
+MIDDLEWARE = (
+    [
+        "django.middleware.security.SecurityMiddleware",
+        "django.contrib.sessions.middleware.SessionMiddleware",
+        "django.middleware.locale.LocaleMiddleware",
+        "django.middleware.common.CommonMiddleware",
+        "django.middleware.csrf.CsrfViewMiddleware",
+        "django.contrib.auth.middleware.AuthenticationMiddleware",
+        "django.contrib.messages.middleware.MessageMiddleware",
+    ]
+    + []
+    if DEBUG
+    else [
+        # clickjacking protection is incompatible with webviews, like vscode's simple browser
+        "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    ]
+)
+
 
 ROOT_URLCONF = "skoufas_library_project.urls"
 
