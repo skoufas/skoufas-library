c12471669b58402689b4b81c58c25c388810f9fa
Claudio Bantaloukas <rockdreamer@gmail.com>
2023-02-18 14:54:56 +0000

    Fix settings

--- skoufas_library_project/settings.py
+++ skoufas_library_project/settings.py
@@ -56,37 +56,35 @@ LOGGING = {
 
 # Application definition
 
-INSTALLED_APPS = (
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "bootstrap5",
+    "books",
+    "djangoql",
+    "loaning",
+] + (
     [
-        "django.contrib.admin",
-        "django.contrib.auth",
-        "django.contrib.contenttypes",
-        "django.contrib.sessions",
-        "django.contrib.messages",
-        "django.contrib.staticfiles",
-        "bootstrap5",
-        "books",
-        "djangoql",
-        "loaning",
-    ]
-    + [
         "django_extensions",
     ]
     if DEBUG
     else []
 )
 
-MIDDLEWARE = (
-    [
-        "django.middleware.security.SecurityMiddleware",
-        "django.contrib.sessions.middleware.SessionMiddleware",
-        "django.middleware.locale.LocaleMiddleware",
-        "django.middleware.common.CommonMiddleware",
-        "django.middleware.csrf.CsrfViewMiddleware",
-        "django.contrib.auth.middleware.AuthenticationMiddleware",
-        "django.contrib.messages.middleware.MessageMiddleware",
-    ]
-    + []
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+] + (
+    []
     if DEBUG
     else [
         # clickjacking protection is incompatible with webviews, like vscode's simple browser
