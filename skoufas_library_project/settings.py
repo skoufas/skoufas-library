f275252a8460d256cf71f15fafaf9ac9b179b308
Claudio Bantaloukas <rockdreamer@gmail.com>
2025-12-31 10:59:23 +0000

    Use django-bootstrap5

--- skoufas_library_project/settings.py
+++ skoufas_library_project/settings.py
@@ -63,7 +63,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "bootstrap5",
+    "django_bootstrap5",
     "books",
     "djangoql",
     "loaning",
